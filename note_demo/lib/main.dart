475be2a1a0085f7a172308fdaab89b9bb3bf47fd
Duy <vietduy1512@gmail.com>
2020-08-07 00:05:47 +0700

    2. Stateless and Stateful Widget

--- note_demo/lib/main.dart
+++ note_demo/lib/main.dart
@@ -6,15 +6,31 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: Text('Hello World'),
-        ),
+      title: 'Todo Note',
+      theme: ThemeData(primaryColor: Colors.green),
+      home: HomePage(title: 'Todo Note'),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _HomePageState createState() => new _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
       ),
+      body: Center(
+        child: Text(widget.title),
+      )
     );
   }
 }
